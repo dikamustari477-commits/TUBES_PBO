920c29331fab685780c9a095d5c3c36c828a6475
Andika Jusuf Jeremy Mustari <235107796+dikamustari477-commits@users.noreply.github.com>
2026-06-04 12:28:45 +0800

    Add password field to ModelPenjahit class

--- ModelPenjahit.java
+++ ModelPenjahit.java
@@ -4,14 +4,17 @@ public class ModelPenjahit {
     private String nama;
     private String noHp;
     private String alamat;
+    private String password;
 
     public ModelPenjahit(String nama, String noHp, String alamat) {
         this.nama   = nama;
         this.noHp   = noHp;
         this.alamat = alamat;
+        this.password = passwword;
     }
 
     public String getNama()     { return nama; }
     public String getNoHp()     { return noHp; }
     public String getAlamat()   { return alamat; }
+    public String getPassword() { return password; }
 }
