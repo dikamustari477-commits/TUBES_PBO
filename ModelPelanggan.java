240eae9caa2172f2ff8d8d6a9050d9315e03343f
Andika Jusuf Jeremy Mustari <235107796+dikamustari477-commits@users.noreply.github.com>
2026-06-04 12:30:35 +0800

    Add password field and update constructor

--- ModelPelanggan.java
+++ ModelPelanggan.java
@@ -4,26 +4,29 @@ public class ModelPelanggan {
     private String nama;
     private String noHp;
     private String alamat;
+    private String password: 
     private String statusPesanan;   
     private String statusPembayaran; 
 
-    // Constructor 
-    public ModelPelanggan(String nama, String noHp, String alamat) {
+    // Cons
+    public ModelPelanggan(String nama, String noHp, String alamat, String password) {
         this.nama = nama;
         this.noHp = noHp;
         this.alamat = alamat;
+        this.password = password;
         this.statusPesanan = "Pending";     
         this.statusPembayaran = "Belum Bayar"; 
     }
 
-    // Getter
+    // Get
     public String getNama() { return nama; }
     public String getNoHp() { return noHp; }
     public String getAlamat() { return alamat; }
+    public String getPassword() { return password; }
     public String getStatusPesanan() { return statusPesanan; }
     public String getStatusPembayaran() { return statusPembayaran; }
 
-    // Setter 
+    // Set
     public void setStatusPesanan(String statusPesanan) { this.statusPesanan = statusPesanan; }
     public void setStatusPembayaran(String statusPembayaran) { this.statusPembayaran = statusPembayaran; }
 }
