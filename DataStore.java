818ee92edb10b1de3bf7e0b61fb7f854565f0b4f
Andika Jusuf Jeremy Mustari <235107796+dikamustari477-commits@users.noreply.github.com>
2026-06-15 10:17:52 +0800

    Delete DataStore.java

--- DataStore.java
+++
@@ -1,8 +0,0 @@
-package tubes_pbo;
-
-import java.util.ArrayList;
-
-public class DataStore {
-    public static ArrayList<ModelPelanggan> listPelanggan   = new ArrayList<>();
-    public static ArrayList<ModelPenjahit> listPenjahit     = new ArrayList<>(); 
-}
